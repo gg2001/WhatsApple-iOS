ec3d576ac36cd4dc814c727c6799761c37bd9d95
gg2001 <gautham@gggcubed.com>
2019-03-24 11:05:16 +1100

    Initial Commit

---
+++ WhatsApple-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatsApple-iOS
+//
+//  Created by gg on 3/24/19.
+//  Copyright © 2019 gg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
